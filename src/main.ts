ef06f863fd76edf5b938d08d48f309bc838d9558
maeda <mashumaro1997@icloud.com>
2024-03-31 09:32:02 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,81 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false, // セキュリティ向上のため、可能な限り無効にしましょう
+      contextIsolation: true, //preload スクリプトのセキュリティ向上のために
+      webSecurity: true,  // CSP を有効化する
+    },
+  });
+
+  // and load the index.html of the app.
+  if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
+  } else {
+    mainWindow.loadFile(path.join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`));
+  }
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+//セキュリティの設定
+import * as crypto from 'crypto';
+
+const generateNonce = (): string => {
+  return crypto.randomBytes(16).toString('base64');
+};
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
+
+import os from 'os';
+
+app.on('ready', () => {
+  app.commandLine.appendSwitch('disable-features', 'Eval');
+  // CPU 情報
+  const cpus = os.cpus();
+  console.log('CPU:', cpus.length);
+
+  // メモリ情報
+  const freemem = os.freemem();
+  const totalmem = os.totalmem();
+  console.log('メモリ:', (freemem / totalmem * 100).toFixed(2) + '%');
+
+  // ...
+});
